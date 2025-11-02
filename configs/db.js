ab72e67c68ca8cf348c8b52fa8e9588910762ca1
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 <syed.anwar@golootlo.pk>
2025-11-02 16:27:05 +0500

    connect mongodb

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/file-sharing-app")
+    .connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to local MongoDB"))
     .catch((err) => console.error("Could not connect to local MongoDB:", err));
 
