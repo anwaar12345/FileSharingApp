bdd9df889c31c61b215c6c79ed25c6fd78cd1e9a
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 <syed.anwar@golootlo.pk>
2025-11-02 16:22:04 +0500

    mongoose integration

---
+++ configs/db.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/file-sharing-app")
+    .then(() => console.log("Connected to local MongoDB"))
+    .catch((err) => console.error("Could not connect to local MongoDB:", err));
+
+  mongoose.connection.on("connected", () => {
+    console.log("Mongoose default connection open");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.error("Mongoose default connection error:", err);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Mongoose default connection disconnected");
+  });
+
+  // Close the Mongoose connection when the Node.js process exits
+  process.on("SIGINT", () => {
+    mongoose.connection.close(() => {
+      console.log(
+        "Mongoose default connection disconnected through app termination"
+      );
+      process.exit(0);
+    });
+  });
+};
+
+export default connectDB;
