f05f160ff97c6f1127d3c0438bec6d5d16133121
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 <syed.anwar@golootlo.pk>
2025-11-02 16:01:44 +0500

    default port added

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ dotenv.config();
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
