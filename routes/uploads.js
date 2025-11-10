7ecdc747c16fa0eff97545dc8853fcfe49cccc4a
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 <syed.anwar@golootlo.pk>
2025-11-10 13:13:50 +0500

    rendering kick start

--- routes/uploads.js
+++ routes/uploads.js
@@ -36,4 +36,23 @@ router.post("/", (req, res) => {
   });
 });
 
+router.get("/:uuid", async (req, res) => {
+  try {
+    const file = await File.findOne({ uuid: req.params.uuid });   
+    if(!file){
+      return res.render("download", { error: "Link has expired or is invalid." });
+    }
+    return res.render("download", {
+      uuid: file.uuid,
+      fileName: file.fileName,
+      fileSize: file.size,
+      downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`,
+    });
+
+  }catch (err) {
+    return res.render("download", { error: "Something went wrong." });
+  }
+
+});
+
 export default router;
